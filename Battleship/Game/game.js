e7c514c956c45f99e0ca5f784a57f59f21fc026a
annmaryvinod <annmarypriyavinod@gmail.com>
2021-10-22 11:28:04 +0530

    Game

--- Battleship/Game/game.js
+++ Battleship/Game/game.js
@@ -67,7 +67,53 @@ var model = {
     }
     return true;
   },
+  generateShipLocations: function () {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+  generateShip: function () {
+    var direction = Math.floor(Math.random() * 2);
+    var row;
+    var col;
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(
+        Math.random() * (this.boardSize - (this.shipLength + 1))
+      );
+    } else {
+      row = Math.floor(
+        Math.random() * (this.boardSize - (this.shipLength + 1))
+      );
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push(row + i + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+  collision: function (locations) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      for (var j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 };
+
 
 // Test for model
 
@@ -155,12 +201,23 @@ var controller = {
 function init() {
   var fireButton = document.getElementById("fireButton");
   fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
 }
 function handleFireButton() {
   var guessInput = document.getElementById("guessInput");
   var guess = guessInput.value;
   controller.processGuess(guess);
   guessInput.value = "";
+}
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
 }
 window.onload = init;
 
