d4a990bf374f51ad01e37e7f0fd5059feb066b57
annmaryvinod <annmarypriyavinod@gmail.com>
2022-09-23 13:39:10 +0530

    html template

---
+++ HTML template/one.js
@@ -0,0 +1,12 @@
+
+const title = "The Big Bang Theory";
+const author = "Bill Bryson";
+const likes =90;
+
+
+let html = `
+<h2>${title}</h2>
+<p>By ${author}</p>
+<span>This blog has ${likes} likes</span>`;
+
+document.write(html);
