b560e32657064da5e27cc40fd9f04e38446c4812
annmaryvinod <annmarypriyavinod@gmail.com>
2022-09-23 13:33:08 +0530

    template strings

---
+++ Template Strings/one.js
@@ -0,0 +1,13 @@
+// Template strings are ways to concatenate two or more strings without the usual concatenation operator +
+
+const title = "The Big Bang Theory";
+const author = "Bill Bryson";
+const likes =90;
+
+const result = `The blog called ${title} by ${author} has ${likes} likes.`;
+console.log(result);
+
+
+
+
+// Template strings use `` to take the content and ${} to take the variable identifier
