322eda0b54bc19da25860d47414f4ce5016a9bdd
annmaryvinod <annmarypriyavinod@gmail.com>
2022-09-23 14:20:55 +0530

    type conversion

--- Z-external/Type Conversion/one.js
+++ Z-external/Type Conversion/one.js
@@ -8,3 +8,35 @@ console.log(num+1);  // Gives 4561 (concatenation)
 
 let score = Number(num);
 console.log(score+1);  //Gives 457
+
+
+
+let result = 'hello';
+console.log(Number(result));  // Gives NaN
+
+
+let dig = 90;
+console.log(typeof(dig));  //number
+
+let kim = String(dig);
+console.log(typeof(kim)); //string
+
+
+
+
+let bool = Boolean(90);
+console.log(bool);   // true since positive
+console.log(Boolean(0)); //false since 0
+console.log(Boolean(-90)); // true since negative
+
+console.log(Boolean('0')); //true since string
+console.log(Boolean('')); // false since empty string
+
+
+
+
+
+// Note 
+
+// Strings of all length has truth value but empty string is false
+// All numbers except 0 has true value
